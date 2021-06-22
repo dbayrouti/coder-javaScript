7875f94ec97809df3f5ca683ab278e92d1a66536
Dany Bayrouti <dbayrouti@gmail.com>
2021-06-22 15:25:07 -0400

    se sube primer desafío del curso de JavaScript

---
+++ javaScript.js
@@ -0,0 +1,15 @@
+// Primer Algoritmo
+// const saludo = prompt("Ingrese Nombre");
+// console.log("Hola " + saludo);
+
+// Segundo Algoritmo
+// const pedirNumero = parseInt(prompt("Ingrese número"))
+// const numero = 25;
+// console.log(pedirNumero + numero)
+// const suma = pedirNumero + numero;
+// console.log(suma);
+
+// Tercer Algoritmo
+// const ingreseNombre = prompt("Ingrese su nombre");
+// const ingreseSuApellido = prompt("Ingrese su apellido");
+// alert("Bienvenid@ " + ingreseNombre + " " + ingreseSuApellido);
